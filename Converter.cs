9c010bfbf3f64069b49ab8f016ec67b2c0879ee2
Иван Сарычев <siaman@mail.ru>
2024-02-02 21:46:39 +0300

    fixes for Converter.ToPaymaster()

--- Converter.cs
+++ Converter.cs
@@ -7,55 +7,86 @@
             var result = new Paymaster();
             foreach (var operation in smartSms.Operations)
             {
-                if (!operation.AffectStatistics) continue;
+                if (!operation.AffectStatistics)
+                    continue;
+
                 var transaction = new Paymaster.Transaction
                 {
-                    AccountName = string.Join(' ', operation.AccountName, operation.CardNumber),
+                    AccountName = ParseAccountName(operation),
                     Category = operation.Category,
                     Comment = operation.SmsText,
                     Currency = operation.TransactionCurrency,
                     Date = DateOnly.FromDateTime(operation.Date.Date),
                     Sum = operation.TransactionAmount,
-                    Type = OperationKindToTransactionType(operation.OperationKind ?? operation.OperationType),
+                    Type = operation.TransactionAmount > 0 ? "Income" : "Expense",
                     Tag = string.Empty
                 };
-                result.Transactions.Add(transaction);
-                var existedAccount = result.Accounts.FirstOrDefault(x => x.Name == transaction.AccountName);
-                if (existedAccount == null)
+
+                if (UpdateTransactions(result, transaction))
                 {
-                    var account = new Paymaster.Account
-                    {
-                        Name = transaction.AccountName,
-                        Currency = operation.Currency,
-                        Sum = operation.Balance,
-                    };
-                    result.Accounts.Add(account);
-                }
-                else
-                {
-                    existedAccount.Sum = operation.Balance;
+                    UpdateAccounts(result, transaction, operation.Balance);
                 }
             }
             return result;
         }
 
-        private static string OperationKindToTransactionType(string operationKind) 
+        private static string ParseAccountName(SmartSms.Operation operation)
         {
-            return operationKind.IsAnyOf("EXPENSE", "TRANSFER_OUT", "CASH_WITHDRAWAL")
-                ? "Expense"
-                : operationKind.IsAnyOf("TRANSFER_IN", "CASH_TRANSACTION", "CASH_BACK", "REVERSAL")
-                ? "Income"
-                : throw new Exception("Unexpected OperationKind: " + operationKind);
+            if (!string.IsNullOrEmpty(operation.SmsText))
+            {
+                var index = operation.SmsText.IndexOf('*');
+                var cardName = operation.SmsText.Contains("Карта") ? "Карта"
+                             : operation.SmsText.Contains("Счет") ? "Счет"
+                             : operation.AccountName;
+                return string.Join(' ', cardName, operation.SmsText.Substring(index, 5));
+            }
+            if (operation.OperationType == "CASH_TRANSACTION" || operation.AccountName == "My account")
+            {
+                return "Мой кошелек";
+            }
+            return operation.AccountName;
         }
 
-        private static bool IsAnyOf(this string text, params string[] values)
+        private static bool UpdateTransactions(Paymaster result, Paymaster.Transaction transaction)
         {
-            foreach (var item in values)
+            if (string.IsNullOrWhiteSpace(transaction.AccountName))
             {
-                if (text == item)
-                    return true;
+                Console.WriteLine($"Could not define Account name. Operation date: {transaction.Date}, sum: {transaction.Sum}");
+                return false;
             }
-            return false;
+
+            if (transaction.Type == "Income" && string.IsNullOrEmpty(transaction.Comment) && transaction.Category != "Зарплата")
+                return false;
+
+            if (transaction.Category == "Перевод" && string.IsNullOrEmpty(transaction.Comment))
+                return false;
+
+            result.Transactions.Add(transaction);
+            return true;
+        }
+
+        private static void UpdateAccounts(Paymaster result, Paymaster.Transaction transaction, double balance)
+        {
+            if (string.IsNullOrEmpty(transaction.AccountName))
+                return;
+
+            var existedAccount = result.Accounts.Find(x => x.Name == transaction.AccountName);
+
+            if (existedAccount == null)
+            {
+
+                var account = new Paymaster.Account
+                {
+                    Name = transaction.AccountName,
+                    Currency = transaction.Currency,
+                    Sum = balance,
+                };
+                result.Accounts.Add(account);
+            }
+            else
+            {
+                existedAccount.Sum = balance;
+            }
         }
     }
 }
